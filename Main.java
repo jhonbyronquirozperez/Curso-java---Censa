816722c6671e9eafac5a4e40d07b6483eb339b65
jh0nbyron <49107469+jhonbyronquirozperez@users.noreply.github.com>
2021-09-01 01:01:24 +0000

    2. Realizar un programa que permita ingresar un número. El programa deberá representar las tablas de multiplicar

--- Main.java
+++ Main.java
@@ -1,32 +1,21 @@
+import java.util.Scanner;
+
 class Main {
 public static void main(String[] args) {
   
-  int A = 4538, C1, C2, C3, C4;
-  String c1, c2, c3, c4;
+Scanner entrada = new Scanner(System.in);
+  int numero;
 
-  C4 = A % 10;
+System.out.println("ingrese un numero: ");
+numero = entrada.nextInt();  
 
-  System.out.println("c4 = "+C4);
+System.out.println("el numero ingresado es : "+numero);
 
- C3 = A % 100 / 10;
-
-  System.out.println("C3 = "+C3);
-
-C2 = A % 1000 / 100;
-
-  System.out.println("C2 = "+C2);
-
-C1 = A % 10000 /1000;
-
-  System.out.println("C1 = "+C1);
-
-c1 = String.valueOf(C1);
-c2 = String.valueOf(C2);
-c3 = String.valueOf(C3);
-c4 = String.valueOf(C4);
-
-System.out.println("El numero "+ A +" ahora es ");
-System.out.println(c4+c3+c2+c1);
+System.out.println(1*numero);
+System.out.println(2*numero);
+System.out.println(3*numero);
+System.out.println(4*numero);
+System.out.println(5*numero);
 
 
   }
